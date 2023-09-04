7f5ee62a6db568b72afba02fa90ff9b78c2f444e
Brendan Sikorjak <brendansikorjak@Brendans-MBP.fios-router.home>
2023-09-04 19:02:30 -0400

    added circle logo gen

--- generateLogo.js
+++ generateLogo.js
@@ -14,6 +14,17 @@ function generateLogo(userInput) {
       </svg>
         `;
     return svgString;
+  } else if (shape === 'circle') {
+    const svgString = `
+      <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="150" cy="100" r="80" fill="${shapeColor}" />
+  
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">
+          ${text}
+        </text>
+      </svg>
+      `;
+    return svgString;
   }
 }
 
